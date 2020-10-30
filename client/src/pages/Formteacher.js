d54b92e45dadda557973644162108154274928be
Gerardo Ezequiel <geraezemc@gmail.com>
2020-10-30 15:55:53 +0000

    Adding some style and generating the forms

--- client/src/pages/Formteacher.js
+++ client/src/pages/Formteacher.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FormTeacher = () => {
   return (
     <main>
-      <h2>New</h2>
+      <h2>Introduzca el nuevo profesor</h2>
 
       <form>
         <label>Nombre:</label>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 const Home = () => {
   return (
     <main>
-      <h2>Use los enlaces de la cabecera para ir a las listas</h2>
+      <h1>¿Quieres aprender programación web, pero no sabes como empezar?</h1>
+      <p>¡Aprende a programar con nuestros cursos!</p>
+      <p>Tenemos cursos para aprender sobre HTML, CSS, y Javascript.</p>
+      <button onClick="#"><Link to="/curso">Lista de Cursos</Link></button>
     </main>
   );
 };
